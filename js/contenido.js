ec7468017693de50cd8ec88d8a5063b5e0c584ed
DESKTOP-QM20VVN\Salgado_Laptop <salgadosb1986@gmail.com>
2025-08-07 21:25:27 -0500

    Update princess r21

--- js/contenido.js
+++ js/contenido.js
@@ -8,38 +8,38 @@ const textos = [
     "Yo quiero que tú seas mi princesa, que tú seas la bella y yo la bestia",
     "Y solo con tus besos me iluminas y me llenas",
     "Y que despiertes de tus sueños solo con mis besos",
-    "¿Quien es mi princesa?"
+    "¿Quiéres saber que paso recorriendo otros reinos?"
 ];
 
 // Elemento donde se va a cambiar el texto
 const descripcion = document.getElementById('descripcion-text');
+const descripcion_a = document.getElementById('descripcion-text-a');
 const ourSpace = document.getElementById('our_space');
 
 // Función para cambiar el texto de acuerdo a la imagen activa
 document.getElementById('carouselExampleAutoplaying').addEventListener('slide.bs.carousel', function (event) {
-    // Cambiar el texto de acuerdo al índice de la imagen activa
+    // Obtener el índice de la imagen activa
     let index = event.to;  // Obtiene el índice de la imagen activa (comienza desde 0)
-
-    // Si el índice excede el número de textos, lo volvemos a ajustar a 0
+    
+    // Asegúrate de que el índice esté dentro del rango correcto
     if (index >= textos.length) {
         index = 0;
     }
 
     // Cambiar el texto según el índice de la imagen
-    descripcion.textContent = textos[index];
+    descripcion.textContent = textos[index]; // Aquí cambiamos el texto de la descripción
 
-       if (index === textos.length - 1) {
-        // Verificar si ya hemos insertado el contenido en #our_space, para evitar duplicados
-        if (!ourSpace.innerHTML.includes('Recorri cada reino y obtuve algo para ti')) {
-            const extraText = document.createElement('p');
-            extraText.id = 'descripcion-text';
-            extraText.innerHTML = `Pulsame y mira lo que adquiri Recorriendo cada reino para ti: <a href="pages/vida.html">Vamos!</a>`;
-            ourSpace.appendChild(extraText);
-        }
+    // Verificar si ya hemos insertado el contenido en #our_space, para evitar duplicados
+    if (index === textos.length - 1) {
+        // Cambiar el texto del párrafo por el nuevo contenido
+        descripcion_a.innerHTML = `Pulsame y mira lo que adquirí recorriendo cada reino para ti: <a href="pages/vida.html">¡Vamos!</a>`;
+    } else {
+        // Volver al texto original si no estamos en el último texto
+        descripcion_a.textContent = "Leeme hasta el final";
     }
-    
 });
 
+// Reproducir el audio con el botón
 document.addEventListener('DOMContentLoaded', function () {
     const btn = document.getElementById('btnPlayAudio');
     const audio = document.getElementById('audio-cuento');
