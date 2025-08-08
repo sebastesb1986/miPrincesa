5404dae5743cd30294a0933a1cf756ab9219fb96
DESKTOP-QM20VVN\Salgado_Laptop <salgadosb1986@gmail.com>
2025-08-07 20:33:19 -0500

    Update princess

--- js/contenido.js
+++ js/contenido.js
@@ -1,26 +1,19 @@
 // Textos para cada imagen
 const textos = [
-    "Érase una vez, hace muchos años, cuando te vi por primera vez.",
-    "Muchos años después, una amena charla entre risas y una simple pregunta: “¿Estás bien?” se convirtió en la entrada hacia algo muy especial.",
-    "A medida que pasaban los días, un día me preguntaste si te veías mejor en blanco y negro o a color. Yo respondí, sin dudarlo, que te veías como la mejor.",
-    "Sin darnos cuenta, el tiempo pasaba, y entre un caballero y esta hermosa princesa algo comenzó a florecer... hasta que llegó el día de su cumpleaños.",
-    "Y un pequeño detalle llegó a sus manos de forma imprevista, alegrando inmensamente la jornada de la princesa.",
-    "Aquel caballero hizo una aparición ante su princesa, y desde la distancia percibió el amor que nacía entre ellos... y le dio un nombre: VIDA.",
-    "Juntos crearon algo llamado 'Nuestro Espacio', donde se imaginaban cómo se verían compartiendo la vida, uno al lado del otro.",
-    "Pasaban los días, y la princesa sonreía más. Sus ojos brillaban con una luz especial, y su caballero podía leer en ellos lo feliz que ella se sentía.",
-    "El caballero la admiraba en cada instante. Hablaban durante horas y horas, acompañándose en decenas de madrugadas.",
-    "Mientras los días seguían su curso, la princesa le contaba sus sueños, y cómo se imaginaba viviendo momentos especiales a su lado.",
-    "Hasta que un día, el caballero decidió organizar su vida, y con determinación fue a visitarla. Se encontraron, fueron juntos a la casita de Dios, y allí surgió una química especial, una conexión profunda.",
-    "Desde entonces, se miran como uno solo. Son felices, sonrientes, y comparten incluso los más pequeños detalles... como una auténtica princesa y su caballero.",
-    "Su amor es evidente, limpio, sano y honesto. Y un día, sellaron tanta espera, ansiedad y sorpresa con una relación: 16-06-2025.",
-    "No había dragones que combatir, ni nadie que rescatar, pero el caballero juró proteger a su princesa mientras su amor y sus fuerzas se lo permitieran.",
-    "Algo hermoso comenzó a cambiar en ella... La princesa se volvió aún más elegante, especial y bonita. Tanto, que su caballero decidió retratarla con amor, para el mundo... y para su corazón.",
-    "Hasta el sol de hoy, ella brilla más que nunca. Es la luz que ilumina el desenlace de esta historia.",
-    "Y aunque parece un final, esta historia... no termina aquí. Al decir verdad, apenas comienza. Continuará..."
+    "Yo quiero que seas como cenicienta",
+    "Ser aquel que ponga en tu pie, la zapatilla",
+    "Como un cuento de hadas, te quiero contar todo lo que siento",
+    "Esa mirada dulce y mágica a mí me congela",
+    "Quisiera ser el genio que concede tus deseos",
+    "Yo quiero que tú seas mi princesa, que tú seas la bella y yo la bestia",
+    "Y solo con tus besos me iluminas y me llenas",
+    "Y que despiertes de tus sueños solo con mis besos",
+    "¿Quien es mi princesa?"
 ];
 
 // Elemento donde se va a cambiar el texto
 const descripcion = document.getElementById('descripcion-text');
+const ourSpace = document.getElementById('our_space');
 
 // Función para cambiar el texto de acuerdo a la imagen activa
 document.getElementById('carouselExampleAutoplaying').addEventListener('slide.bs.carousel', function (event) {
@@ -34,6 +27,17 @@ document.getElementById('carouselExampleAutoplaying').addEventListener('slide.bs
 
     // Cambiar el texto según el índice de la imagen
     descripcion.textContent = textos[index];
+
+       if (index === textos.length - 1) {
+        // Verificar si ya hemos insertado el contenido en #our_space, para evitar duplicados
+        if (!ourSpace.innerHTML.includes('Recorri cada reino y obtuve algo para ti')) {
+            const extraText = document.createElement('p');
+            extraText.id = 'descripcion-text';
+            extraText.innerHTML = `Recorri cada reino y obtuve algo para ti: <a href="pages/vida.html">Vamos!</a>`;
+            ourSpace.appendChild(extraText);
+        }
+    }
+    
 });
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -55,4 +59,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
-
